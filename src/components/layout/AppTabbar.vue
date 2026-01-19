2cfdf392476e694bfc23967b5b75fd3f88fe38f1
diaochan <diaochan@seatent.com>
2026-01-19 17:15:19 +0800

    样式调整

--- src/components/layout/AppTabbar.vue
+++ src/components/layout/AppTabbar.vue
@@ -1,10 +1,18 @@
+<!--
+ * @Author: diaochan diaochan@seatent.com
+ * @Date: 2026-01-17 19:13:41
+ * @LastEditors: diaochan diaochan@seatent.com
+ * @LastEditTime: 2026-01-19 17:09:50
+ * @FilePath: /ailimo_h5/src/components/layout/AppTabbar.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <van-tabbar
     v-model="activeTab"
     :fixed="true"
     :placeholder="true"
     :safe-area-inset-bottom="true"
-    active-color="#07C160"
+    active-color="#00e9aa"
   >
     <van-tabbar-item
       v-for="item in tabbarList"
